50c8a41556812f6f8f117d02801c171a79480dcc
anoakblock <169567417+baihuamen@users.noreply.github.com>
2025-07-06 14:18:39 +0800

    添加服务端的支持，添加事件系统，命令系统

--- src/main/java/com/github/baihuamen/skillpractise/Skillpractise.java
+++ src/main/java/com/github/baihuamen/skillpractise/Skillpractise.java
@@ -1,10 +1,14 @@
 package com.github.baihuamen.skillpractise;
 
+import com.github.baihuamen.skillpractise.command.ServerCommandManager;
+import com.github.baihuamen.skillpractise.event.ServerEventManager;
 import net.fabricmc.api.ModInitializer;
 
 public class Skillpractise implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ServerEventManager serverEventManager = new ServerEventManager();
+        ServerCommandManager serverCommandManager = new ServerCommandManager();
     }
 }
